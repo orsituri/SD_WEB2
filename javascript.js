dd6a17c825ad46b8e3a12b86edbc2f371a7411e4
orsituri <155663422+orsituri@users.noreply.github.com>
2024-04-25 16:15:15 +0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -5,17 +5,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
-
-async function addDistrictsGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const polygons = L.geoJson(data{
- onEachFeature: popUPinfo,
-  style: polygonStyle,
- })
- polygons.addTo(map)
-}
-addDistrictsGeoJson('tartu_city_districts_edu.geojson')
 function getColor(property) {
  switch (property) {
  case 1:
@@ -39,6 +28,17 @@ function polygonStyle(feature) {
  opacity: 1,
  color: 'grey',
  }
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data{
+ onEachFeature: popUPinfo,
+  style: polygonStyle,
+ })
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('tartu_city_districts_edu.geojson')
+
 async function addCelltowersGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
